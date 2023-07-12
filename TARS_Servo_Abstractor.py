82c44f770b7777b559bcb4523a0bd4ceb4c2a29c
physicsenjoyer <124938414+physicsenjoyer@users.noreply.github.com>
2023-07-12 04:46:24 -0700

    Add files via upload

--- TARS_Servo_Abstractor.py
+++ TARS_Servo_Abstractor.py
@@ -2,37 +2,19 @@ import time
 import TARS_Servo_Controller
 
 def stepForward():
-	TARS_Servo_Controller.neut_to_up()
-	TARS_Servo_Controller.shift_neut_to_forward()
-	time.sleep(1.5)
-	TARS_Servo_Controller.up_to_down_fast()
-	time.sleep(0.5)
-	TARS_Servo_Controller.shift_forward_to_neut()
-	time.sleep(0.1)
-	TARS_Servo_Controller.down_to_neut()
-
+	TARS_Servo_Controller.height_neutral_to_up()
+	TARS_Servo_Controller.torso_neutral_to_forwards()
+	TARS_Servo_Controller.torso_bump()
+	TARS_Servo_Controller.torso_return()
+	
 def turnRight():
-	TARS_Servo_Controller.neut_to_down()
-	TARS_Servo_Controller.legsTurnRight()
-	TARS_Servo_Controller.down_to_neut()
-	TARS_Servo_Controller.legsFromRtoN()
+	TARS_Servo_Controller.neutral_to_down()
+	TARS_Servo_Controller.turn_right()
+	TARS_Servo_Controller.down_to_neutral()
+	TARS_Servo_Controller.neutral_from_right()
 
 def turnLeft():
-	TARS_Servo_Controller.neut_to_down()
-	TARS_Servo_Controller.legsTurnLeft()
-	TARS_Servo_Controller.down_to_neut()
-	TARS_Servo_Controller.legsFromLtoN()
-
-def pose():
-	TARS_Servo_Controller.neut_to_up()
-	TARS_Servo_Controller.shift_neut_to_forward()
-	TARS_Servo_Controller.up_to_down_fast()
-	
-def unPose():
-	TARS_Servo_Controller.down_to_up_fast()
-	TARS_Servo_Controller.shift_forward_to_midmod()
-	TARS_Servo_Controller.up_to_down_fast()
-	TARS_Servo_Controller.shift_midmod_to_neutral()
-	TARS_Servo_Controller.down_to_neut()
-
-
+	TARS_Servo_Controller.neutral_to_down()
+	TARS_Servo_Controller.turn_left()
+	TARS_Servo_Controller.down_to_neutral()
+	TARS_Servo_Controller.neutral_from_left()
