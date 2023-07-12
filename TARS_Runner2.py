b8551cb5bd6104964074fd8a9e77ebb78c6703b9
physicsenjoyer <124938414+physicsenjoyer@users.noreply.github.com>
2023-07-12 04:42:17 -0700

    Update TARS_Runner2.py

--- TARS_Runner2.py
+++ TARS_Runner2.py
@@ -1,166 +1,65 @@
-def up_to_down_fast():
-	servo0 = servo0Up
-	
-	while (servo0 < servo0Down):
-		servo0 = servo0 + 15
-		pwm.set_pwm(0, 0, servo0)
-		print('Setting servos to: Servo 0: ', servo0)
-		time.sleep(0.0001)
-		
-def down_to_up_fast():
-	servo0 = servo0Down
-	
-	while (servo0 > servo0Up):
-		servo0 = servo0 - 1
-		pwm.set_pwm(0, 0, servo0)
-		print('Setting servos to: Servo 0: ', servo0)
-		time.sleep(0.001)
-		
-def shift_neut_to_forward():
-	servo1 = servo1Neutral
-	servo2 = servo2Neutral
-	
-	while (servo1 < servo1Forward):
-		servo1 = servo1 + 1
-		servo2 = servo2 - 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.000001)
-		
-def shift_forward_to_back():
-	servo1 = servo1Forward
-	servo2 = servo2Forward
-	
-	while (servo1 > servo1Back):
-		servo1 = servo1 - 2
-		servo2 = servo2 + 2
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.005)
-		
-def shift_forward_to_neut():
-	servo1 = servo1Forward
-	servo2 = servo2Forward
-	
-	while (servo1 > servo1Neutral):
-		servo1 = servo1 - 2
-		servo2 = servo2 + 2
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.001)
-		
-def shift_forward_to_neut_slow():
-	servo1 = servo1Forward
-	servo2 = servo2Forward
-	
-	while (servo1 > servo1Neutral):
-		servo1 = servo1 - 2
-		servo2 = servo2 + 2
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.05)
+import evdev
+import time
+import TARS_Servo_Abstractor
+import TARS_Servo_Controller
+import Final_Countdown
 
-modifier = -30
+from evdev import InputDevice, categorize, ecodes
 
-def shift_forward_to_midmod():
-	servo1 = servo1Forward
-	servo2 = servo2Forward
-	
-	while (servo1 > mid1-modifier):
-		servo1 = servo1 - 1
-		servo2 = servo2 + 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.01)
+gamepad = InputDevice('/dev/input/event3')
 
-def shift_midmod_to_neutral():
-	servo1 = mid1-modifier
-	servo2 = mid2+modifier
-	
-	while (servo1 > servo1Neutral):
-		servo1 = servo1 - 1
-		servo2 = servo2 + 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.01)
+lTrg = 37
+rTrg = 50
+upBtn = 46
+downBtn = 32
+lBtn = 18
+rBtn = 33
+xBtn = 35
+yBtn = 23
+aBtn = 34
+bBtn = 36
+minusBtn = 49
+plusBtn = 24
 
+print(gamepad)
 
-def shift_back_to_mid():
-	servo1 = servo1Back
-	servo2 = servo2Back
-	
-	while (servo1 < mid1):
-		servo1 = servo1 + 1
-		servo2 = servo2 - 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.01)
-	
-def shift_mid_to_neut():
-	servo1 = mid1
-	servo2 = mid2
-	
-	while (servo1 < servo1Neutral):
-		servo1 = servo1 + 1
-		servo2 = servo2 - 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		print('Setting servos to: Servo 1: ', servo1, ' and Servo 2: ', servo2)
-		time.sleep(0.01)
-	
-def legsTurnRight():
-	servo1 = servo1Neutral
-	servo2 = servo2Neutral
-	
-	while (servo1 < mid1):
-		servo1 = servo1 + 1
-		servo2 = servo2 + 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		time.sleep(0.001)
-		
-def legsTurnLeft():
-	servo1 = servo1Neutral
-	servo2 = servo2Neutral
-	
-	while (servo1 > mid1i):
-		servo1 = servo1 - 1
-		servo2 = servo2 - 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		time.sleep(0.001)
+pose = False
 
-def legsFromRtoN():
-	servo1 = mid1
-	servo2 = mid2i
-	
-	while (servo1 > servo1Neutral):
-		servo1 = servo1 - 1
-		servo2 = servo2 - 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		time.sleep(0.05)
-
-def legsFromLtoN():
-	servo1 = mid1i
-	servo2 = mid2
-	
-	while (servo1 < servo1Neutral):
-		servo1 = servo1 + 1
-		servo2 = servo2 + 1
-		pwm.set_pwm(1, 1, servo1)
-		pwm.set_pwm(2, 2, servo2)
-		time.sleep(0.05)
-		
-def holdPose():
-	pwm.set_pwm(0, 0, servo0Down)
-	pwm.set_pwm(1, 1, servo1Forward)
-	pwm.set_pwm(2, 2, servo2Forward)
-
+for event in gamepad.read_loop():
+    if event.type == ecodes.EV_KEY:
+        if event.value == 1:
+            if event.code == lTrg:
+                print("Left Trigger")
+            elif event.code == rTrg:
+                print("Right Trigger")
+            elif event.code == upBtn:
+                print("Up - Step Forward")
+                TARS_Servo_Abstractor.stepForward()
+            elif event.code == downBtn:
+                print("Down")
+            elif event.code == lBtn:
+                print("Left - Turn Left")
+                TARS_Servo_Abstractor.turnLeft()
+            elif event.code == rBtn:
+                print("Right - Turn Right")
+                TARS_Servo_Abstractor.turnRight()
+            elif event.code == xBtn:
+                print("X - Pose")
+                if (pose == False):
+                    TARS_Servo_Abstractor.pose()
+                    pose = True
+                elif (pose == True):
+                    TARS_Servo_Abstractor.unPose()
+                    pose = False
+            elif event.code == yBtn:
+                print("Y")
+            elif event.code == aBtn:
+                print("A")
+            elif event.code == bBtn:
+                print("B - Unpose")
+            elif event.code == plusBtn:
+                print("+")
+            elif event.code == minusBtn:
+                print("-")
+        elif event.value == 0:
+            print("Stop")
