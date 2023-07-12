48bf8c7e95a68d2ba1b90e80d78bd5f4a4462596
physicsenjoyer <124938414+physicsenjoyer@users.noreply.github.com>
2023-07-12 00:27:43 -0700

    Update TARS_Servo_Abstractor.py

--- TARS_Servo_Abstractor.py
+++ TARS_Servo_Abstractor.py
@@ -1,72 +1,38 @@
-import evdev
 import time
-import TARS_Servo_Abstractor
 import TARS_Servo_Controller
-import Final_Countdown
 
-from evdev import InputDevice, categorize, ecodes
+def stepForward():
+	TARS_Servo_Controller.neut_to_up()
+	TARS_Servo_Controller.shift_neut_to_forward()
+	time.sleep(1.5)
+	TARS_Servo_Controller.up_to_down_fast()
+	time.sleep(0.5)
+	TARS_Servo_Controller.shift_forward_to_neut()
+	time.sleep(0.1)
+	TARS_Servo_Controller.down_to_neut()
 
-gamepad = InputDevice('/dev/input/event3')
+def turnRight():
+	TARS_Servo_Controller.neut_to_down()
+	TARS_Servo_Controller.legsTurnRight()
+	TARS_Servo_Controller.down_to_neut()
+	TARS_Servo_Controller.legsFromRtoN()
 
-lTrg = 37
-rTrg = 50
-upBtn = 46
-downBtn = 32
-lBtn = 18
-rBtn = 33
-xBtn = 35
-yBtn = 23
-aBtn = 34
-bBtn = 36
-minusBtn = 49
-plusBtn = 24
+def turnLeft():
+	TARS_Servo_Controller.neut_to_down()
+	TARS_Servo_Controller.legsTurnLeft()
+	TARS_Servo_Controller.down_to_neut()
+	TARS_Servo_Controller.legsFromLtoN()
 
-print(gamepad)
+def pose():
+	TARS_Servo_Controller.neut_to_up()
+	TARS_Servo_Controller.shift_neut_to_forward()
+	TARS_Servo_Controller.up_to_down_fast()
+	
+def unPose():
+	TARS_Servo_Controller.down_to_up_fast()
+	TARS_Servo_Controller.shift_forward_to_midmod()
+	TARS_Servo_Controller.up_to_down_fast()
+	TARS_Servo_Controller.shift_midmod_to_neutral()
+	TARS_Servo_Controller.down_to_neut()
 
-pose = False
-
-for event in gamepad.read_loop():
-	if event.type == ecodes.EV_KEY:
-		if event.value == 1:
-			if event.code == lTrg:
-				print("Left Trigger")
-			elif event.code == rTrg:
-				print("Right Trigger")
-			elif event.code == upBtn:
-				print("Up - Step Forward")
-				TARS_Servo_Abstractor.stepForward()
-			elif event.code == downBtn:
-				print("Down")
-			elif event.code == lBtn:
-				print("Left - Turn Left")
-				TARS_Servo_Abstractor.turnLeft()
-			elif event.code == rBtn:
-				print("Right - Turn Right")
-				TARS_Servo_Abstractor.turnRight()
-			elif event.code == xBtn:
-				print("X - Pose")
-				if (pose == False):
-					TARS_Servo_Abstractor.pose()
-					pose = True
-				elif (pose == True):
-					TARS_Servo_Abstractor.unPose()
-					pose = False
-			elif event.code == yBtn:
-				print("Y")
-			elif event.code == aBtn:
-				print("A")
-			elif event.code == bBtn:
-				print("B - Unpose")
-			elif event.code == plusBtn:
-				print("+")
-			elif event.code == minusBtn:
-				print("-")
-		elif event.value == 0:
-			print("Stop")
-			if (pose == True):
-				TARS_Servo_Controller.holdPose()
-			elif (pose == False):
-				TARS_Servo_Controller.neutralHeight()
-				TARS_Servo_Controller.neutralRotation()
-			
 
